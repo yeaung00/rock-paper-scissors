c3a3961234a331d6c7f19180e835bbfca8d0ef51
yeaung00 <yep.aung00@gmail.com>
2022-05-24 07:48:13 -0700

    Added draw feature

--- rps.js
+++ rps.js
@@ -1,15 +1,12 @@
 function computerPlay() {
     let play = Math.floor((Math.random() * 3) + 1);
     if (play == 1) {
-        console.log("rock");
         return "rock";
     }
     if (play == 2) {
-        console.log("paper");
         return "paper";
     }
     if (play == 3) {
-        console.log("scissor");
         return "scissor";
     }
 }
@@ -17,31 +14,47 @@ function computerPlay() {
 function playerSelect(playerSelection, computerSelection) {
     if (computerSelection == "rock") {
         if (playerSelection == "paper") {
-            console.log("You win")
+            console.log("You win");
             return 1;
         }
+
+        if (playerSelection == "rock") {
+            console.log("Draw, play again.");
+            return -1;
+        }
+
         else {
-            console.log("You lose")
+            console.log("You lose");
             return 0;
         }
     }
     if (computerSelection == "paper") {
         if (playerSelection == "scissor") {
-            console.log("You win")
+            console.log("You win");
             return 1;
         }
+
+        if (playerSelection == "paper") {
+            console.log("Draw, play again.");
+            return -1;
+        }
+
         else {
-            console.log("You lose")
+            console.log("You lose");
             return 0;
         }
     }
     if (computerSelection == "scissor") {
         if (playerSelection == "rock") {
-            console.log("You win")
+            console.log("You win");
             return 1;
         }
+        if (playerSelection == "scissors") {
+            console.log("Draw, play again.");
+            return -1;
+        }
         else {
-            console.log("You lose")
+            console.log("You lose");
             return 0;
         }
     }
@@ -50,13 +63,14 @@ function playerSelect(playerSelection, computerSelection) {
 function game() {
     let playerPoint = 0;
     let computerPoint = 0;
+    let computerSelection = "";
+    let playerSelection = "";
     let points = 0;
     for (let i = 0; i < 5; i++) {
         computerSelection = computerPlay();
         playerSelection = prompt("Choose between rock, paper, or scissors.");
-        while (playerSelection != "rock" || "paper" || "scissors") {
-            playerSelection = prompt("The input value is not accepted. Choose between rock, paper, or scissors.");
-        }
+        console.log("Player chooses " + playerSelection);
+        console.log("Computer chooses " + computerSelection);
         points = playerSelect(playerSelection, computerSelection);
 
         if (points == 1) {
@@ -65,9 +79,23 @@ function game() {
         if (points == 0) {
             computerPoint += 1;
         }
+        if (points == -1) {
+            computerSelection = computerPlay();
+            playerSelection = prompt("Choose between rock, paper, or scissors.");
+            console.log("Player chooses " + playerSelection);
+            console.log("Computer chooses " + computerSelection);
+            points = playerSelect(playerSelection, computerSelection);
+            if (points == 1) {
+                playerPoint += 1;
+            }
+            if (points == 0) {
+                computerPoint += 1;
+            }
+        }
     }
     
-    console.log(playerPoint +':'+computerPoint)
+    console.log("Player: " + playerPoint)
+    console.log("Computer: " + computerPoint)
     if (computerPoint > playerPoint) {
         console.log("Computer wins!")
     }
